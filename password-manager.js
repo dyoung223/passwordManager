4f83df93053d55b2fd8cfb2f14d73deeb5a9979b
dyoung223 <davisy30@gmail.com>
2023-02-12 15:27:11 -0800

    first commit adding base password-manager.js file

---
+++ password-manager.js
@@ -0,0 +1,137 @@
+"use strict";
+
+/********* External Imports ********/
+
+const { byteArrayToString, genRandomSalt, untypedToTypedArray, bufferToUntypedArray } = require("./lib");
+const { subtle } = require('crypto').webcrypto;
+
+/********* Implementation ********/
+class Keychain {
+  /**
+   * Initializes the keychain using the provided information. Note that external
+   * users should likely never invoke the constructor directly and instead use
+   * either Keychain.init or Keychain.load. 
+   * Arguments:
+   *  You may design the constructor with any parameters you would like. 
+   * Return Type: void
+   */
+  constructor(password) {
+    this.data = { 
+      /* Store member variables that you intend to be public here
+         (i.e. information that will not compromise security if an adversary sees) */
+    };
+    this.secrets = {
+      /* Store member variables that you intend to be private here
+         (information that an adversary should NOT see). */
+
+      
+    };
+
+    this.data.version = "CS 255 Password Manager v1.0";
+    // Flag to indicate whether password manager is "ready" or not
+    this.ready = true;
+
+    throw "Not Implemented!";
+  };
+
+  /** 
+    * Creates an empty keychain with the given password. Once the constructor
+    * has finished, the password manager should be in a ready state.
+    *
+    * Arguments:
+    *   password: string
+    * Return Type: void
+    */
+  static async init(password) {
+    KVS = new Keychain(password);
+    
+
+    throw "Not Implemented!";
+  }
+
+  /**
+    * Loads the keychain state from the provided representation (repr). The
+    * repr variable will contain a JSON encoded serialization of the contents
+    * of the KVS (as returned by the dump function). The trustedDataCheck
+    * is an *optional* SHA-256 checksum that can be used to validate the 
+    * integrity of the contents of the KVS. If the checksum is provided and the
+    * integrity check fails, an exception should be thrown. You can assume that
+    * the representation passed to load is well-formed (i.e., it will be
+    * a valid JSON object).Returns a Keychain object that contains the data
+    * from repr. 
+    *
+    * Arguments:
+    *   password:           string
+    *   repr:               string
+    *   trustedDataCheck: string
+    * Return Type: Keychain
+    */
+  static async load(password, repr, trustedDataCheck) {
+    throw "Not Implemented!";
+  };
+
+  /**
+    * Returns a JSON serialization of the contents of the keychain that can be 
+    * loaded back using the load function. The return value should consist of
+    * an array of two strings:
+    *   arr[0] = JSON encoding of password manager
+    *   arr[1] = SHA-256 checksum (as a string)
+    * As discussed in the handout, the first element of the array should contain
+    * all of the data in the password manager. The second element is a SHA-256
+    * checksum computed over the password manager to preserve integrity. If the
+    * password manager is not in a ready-state, return null.
+    *
+    * Return Type: array
+    */ 
+  async dump() {
+    throw "Not Implemented!";
+  };
+
+  /**
+    * Fetches the data (as a string) corresponding to the given domain from the KVS.
+    * If there is no entry in the KVS that matches the given domain, then return
+    * null. If the password manager is not in a ready state, throw an exception. If
+    * tampering has been detected with the records, throw an exception.
+    *
+    * Arguments:
+    *   name: string
+    * Return Type: Promise<string>
+    */
+  async get(name) {
+    throw "Not Implemented!";
+  };
+
+  /** 
+  * Inserts the domain and associated data into the KVS. If the domain is
+  * already in the password manager, this method should update its value. If
+  * not, create a new entry in the password manager. If the password manager is
+  * not in a ready state, throw an exception.
+  *
+  * Arguments:
+  *   name: string
+  *   value: string
+  * Return Type: void
+  */
+  async set(name, value) {
+    throw "Not Implemented!";
+  };
+
+  /**
+    * Removes the record with name from the password manager. Returns true
+    * if the record with the specified name is removed, false otherwise. If
+    * the password manager is not in a ready state, throws an exception.
+    *
+    * Arguments:
+    *   name: string
+    * Return Type: Promise<boolean>
+  */
+  async remove(name) {
+    throw "Not Implemented!";
+  };
+
+  static get PBKDF2_ITERATIONS() { return 100000; }
+};
+
+module.exports = {
+  Keychain: Keychain
+}
